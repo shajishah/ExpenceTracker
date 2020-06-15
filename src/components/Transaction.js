bfd9642a02c65a6d249cfe7bccb97b9fa24ca799
syedshajihaider <syedshajihaider@gmail.com>
2020-06-15 15:37:11 +0500

    tansaction list updated

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,15 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { motion } from "framer";
+
 
 export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
   const { deleteTransaction } = useContext(GlobalContext);
   return (
-    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+    <motion.li
+    whileHover={{ scale: 1.1 }}
+     className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
         {sign}${Math.abs(transaction.amount)}
@@ -16,6 +20,6 @@ export const Transaction = ({ transaction }) => {
       >
         x
       </button>
-    </li>
+    </motion.li>
   );
 };
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,22 +1,19 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import { Transaction } from "./Transaction";
-import { motion } from "framer";
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
   return (
     <>
       <h3>History</h3>
-      <motion.ul
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.8 }}
+      <ul
         className="list"
       >
         {transactions.map((transaction) => (
           <Transaction key={transaction.id} transaction={transaction} />
         ))}
-      </motion.ul>
+      </ul>
     </>
   );
 };
